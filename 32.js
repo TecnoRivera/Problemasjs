6a47a3c553ba0eb684dee28704c52100d2709734
Emiliano Rivera <a358193@uach.mx>
2023-10-29 16:54:17 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,5 @@
+let array = ["Juan","Rodrigo","Joaquin","Pedro","Leonardo","Miguel","Pepe","Victor","Emiliano","Yahir","Luis","Adrian","Uriel","Veronica","Laura","Luisa","Ana","Irving","Melissa","Diego"];
+
+for(let i = 0;i < 20;i++){
+    console.log(array[i]);
+}
